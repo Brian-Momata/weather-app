9a8b503a294a4f4eab27477d2ed258e10784c2e0
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-10-20 11:10:19 +0300

    Update ulr because of Mixed Content Issue

--- src/weather-data.js
+++ src/weather-data.js
@@ -1,5 +1,5 @@
 export default async function weatherData(location) {
-  const url = `http://api.weatherapi.com/v1/current.json?key=9fd44fa461c1448481d85256231710&q=${location}`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=9fd44fa461c1448481d85256231710&q=${location}`;
   const response = await fetch(url);
   const weatherData = await response.json();
 
