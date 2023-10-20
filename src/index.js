f3948fe42396e06f117485016776b8814b18278f
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-10-20 10:58:23 +0300

    Add even listener on form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import fetchWeather from './fetch-weather';
+
+const weatherForm = document.getElementById('weather-form');
+
+weatherForm.onsubmit = event => {
+  const location = document.getElementById('location').value;
+  fetchWeather(location);
+  event.preventDefault();
+};
